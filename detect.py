410db6b7f5126f93d5a1423fd97464e57dd682ac
Zaki Rangwala <zakirangwala@gmail.com>
2020-11-27 23:41:57 -0500

    merge annotations to one file

--- detect.py
+++ detect.py
@@ -35,3 +35,33 @@ def construct_label_map():
             f.write('\tname:\'{}\'\n'.format(label['name']))
             f.write('\tid:{}\n'.format(label['id']))
             f.write('}\n')
+
+# Convert XML files to CSV
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        for member in root.findall('object'):
+            value = (root.find('filename').text+".jpg",
+                     int(root.find('size')[0].text),
+                     int(root.find('size')[1].text),
+                     member[0].text,
+                     int(member[4][0].text),
+                     int(member[4][1].text),
+                     int(member[4][2].text),
+                     int(member[4][3].text)
+                     )
+            xml_list.append(value)
+    column_name = ['filename', 'width', 'height',
+                   'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
+
+def convert():
+    for directory in ['train', 'test']:
+        image_path = f'Tensorflow/workspace/images/{directory}'
+        xml_df = xml_to_csv(image_path)
+        xml_df.to_csv(
+            'Tensorflow/workspace/annotations/{}labels.csv'.format(directory), index=None)
+        print('Successfully converted xml to csv.')

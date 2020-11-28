664ab96435bf303e37e29a44d3aa359a587a1a56
Zaki Rangwala <zakirangwala@gmail.com>
2020-11-27 23:41:13 -0500

    construct label map

--- detect.py
+++ detect.py
@@ -24,3 +24,14 @@ PRETRAINED_MODEL_PATH = WORKSPACE_PATH+'/pre-trained-models'
 CONFIG_PATH = MODEL_PATH+'/my_ssd_mobnet/pipeline.config'
 CHECKPOINT_PATH = MODEL_PATH+'/my_ssd_mobnet/'
 CUSTOM_MODEL_NAME = 'my_ssd_mobnet'
+
+# Label Map
+def construct_label_map():
+    labels = [{'name': 'Mask', 'id': 1}, {'name': 'NoMask', 'id': 2}]
+
+    with open(ANNOTATION_PATH + '\label_map.pbtxt', 'w') as f:
+        for label in labels:
+            f.write('item { \n')
+            f.write('\tname:\'{}\'\n'.format(label['name']))
+            f.write('\tid:{}\n'.format(label['id']))
+            f.write('}\n')

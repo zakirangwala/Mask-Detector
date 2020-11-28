f7b034515ef9ebe5e5a7fadcdfe77a8216ccee77
Zaki Rangwala <zakirangwala@gmail.com>
2020-11-27 23:42:44 -0500

    create tf record + pipeline config

--- detect.py
+++ detect.py
@@ -65,3 +65,32 @@ def convert():
         xml_df.to_csv(
             'Tensorflow/workspace/annotations/{}labels.csv'.format(directory), index=None)
         print('Successfully converted xml to csv.')
+
+# Create TF record
+""" Train : python Tensorflow/scripts/generate_tfrecord.py -x Tensorflow/workspace/images/train -l Tensorflow/workspace/annotations/label_map.pbtxt -o Tensorflow/workspace/annotations/train.record
+Test : python Tensorflow/scripts/generate_tfrecord.py -x Tensorflow/workspace/images/test -l Tensorflow/workspace/annotations/label_map.pbtxt -o Tensorflow/workspace/annotations/test.record """
+
+# Configuration
+def config():
+    CONFIG_PATH = MODEL_PATH+'/'+CUSTOM_MODEL_NAME+'/pipeline.config'
+    config = config_util.get_configs_from_pipeline_file(CONFIG_PATH)
+    pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
+    with tf.io.gfile.GFile(CONFIG_PATH, "r") as f:
+        proto_str = f.read()
+        text_format.Merge(proto_str, pipeline_config)
+    # Modify pipeline
+    pipeline_config.model.ssd.num_classes = 2
+    pipeline_config.train_config.batch_size = 4
+    pipeline_config.train_config.fine_tune_checkpoint = PRETRAINED_MODEL_PATH + \
+        '/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8/checkpoint/ckpt-0'
+    pipeline_config.train_config.fine_tune_checkpoint_type = "detection"
+    pipeline_config.train_input_reader.label_map_path = ANNOTATION_PATH + '/label_map.pbtxt'
+    pipeline_config.train_input_reader.tf_record_input_reader.input_path[:] = [
+        ANNOTATION_PATH + '/train.record']
+    pipeline_config.eval_input_reader[0].label_map_path = ANNOTATION_PATH + \
+        '/label_map.pbtxt'
+    pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [
+        ANNOTATION_PATH + '/test.record']
+    config_text = text_format.MessageToString(pipeline_config)
+    with tf.io.gfile.GFile(CONFIG_PATH, "wb") as f:
+        f.write(config_text)
